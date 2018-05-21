aaab57df7c5302b13c626c4c08842ac4973edf2f
Leonardo Nonnenmacher <leconon@hotmail.com>
2018-05-21 13:45:15 -0300

    Comentários desnecessários
    Removendo comentários desnecessários, para deixar o projeto melhor ainda! :D

--- NetBeansProjects/PICII/src/Main/Main.java
+++ NetBeansProjects/PICII/src/Main/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Main;
 
 /**
@@ -10,10 +5,7 @@ package Main;
  * @author darlan
  */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         System.out.println("issue 3");
     }
